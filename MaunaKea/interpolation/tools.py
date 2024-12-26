5ac82a9150156fe660641bc4928d2e857b253323
Garrett 'Karto' Keating <garrett.keating@cfa.harvard.edu>
2024-12-26 08:14:17 -0500

    Changing DD_CubicHermiteSpline to accept arrays of values

--- MaunaKea/interpolation/tools.py
+++ MaunaKea/interpolation/tools.py
@@ -41,57 +41,41 @@ def DD_CubicHermiteSpline(eval_airmass, eval_nscale, data_dict, reverse=False):
     freq_map = data_dict['freq']['map']
     airmass_jacobian = data_dict['airmass']['jacobian'][::2,::2]
 
-    if reverse:
-        # Interpolate for chosen nscale
-        interp_func = CubicHermiteSpline(
-            x=Nscale_map, y=Tb_scalar_field, dydx=Nscale_jacobian, axis=0
-        )
+    init_interp_func = CubicHermiteSpline(
+        x=Nscale_map if reverse else airmass_map,
+        y=Tb_scalar_field,
+        dydx=Nscale_jacobian if reverse else airmass_jacobian,
+        axis=0 if reverse else 1,
+    )
 
-        Nscale_eval_grid = interp_func(eval_nscale)
-        
-        # Interpolate for airmass Jacobian at the chosen nscale value
-        interp_func = RegularGridInterpolator(
-            points=(Nscale_map, airmass_map, freq_map), values=airmass_jacobian, method="linear"
-        )
-        
-        x,y,z = np.meshgrid(eval_nscale, airmass_map, freq_map, indexing='ij')
-        airmass_jacobian_fixed_nscale = interp_func(
-            (x.flatten(),y.flatten(),z.flatten())
-        ).reshape(Nscale_eval_grid.shape)
+    first_eval = init_interp_func(eval_nscale if reverse else eval_airmass)
 
-        # Interpolate for 2D spectrum using chosen airmass and fixed airmass Jacobian
-        interp_func = CubicHermiteSpline(
-            x=airmass_map, y=Nscale_eval_grid, dydx=airmass_jacobian_fixed_nscale, axis=0
-        )
-        interp_val = interp_func(eval_airmass).reshape(freq_map.shape)
-        
-        return interp_val
-    else:
-        # Interpolate for chosen airmass
-        interp_func = CubicHermiteSpline(
-            x=airmass_map, y=Tb_scalar_field, dydx=airmass_jacobian, axis=1
-        )
+    # Interpolate for nscale Jacobian at the chosen airmass
+    jacob_interp_func = RegularGridInterpolator(
+        points=(Nscale_map, airmass_map, freq_map),
+        values=airmass_jacobian if reverse else Nscale_jacobian,
+        method="linear",
+    )
 
-        airmass_eval_grid = interp_func(eval_airmass)
-        
-        # Interpolate for nscale Jacobian at the chosen airmass
-        interp_func = RegularGridInterpolator(
-            points=(Nscale_map, airmass_map, freq_map), values=Nscale_jacobian, method="linear"
-        )
-        
-        x,y,z = np.meshgrid(Nscale_map, eval_airmass, freq_map, indexing='ij')
-        nscale_jacobian_fixed_airmass = interp_func(
-            (x.flatten(),y.flatten(),z.flatten())
-        ).reshape(airmass_eval_grid.shape)
+    x,y,z = np.meshgrid(
+        eval_nscale if reverse else Nscale_map,
+        airmass_map if reverse else eval_airmass,
+        freq_map,
+        indexing='ij',
+    )
 
-        # Interpolate for 2D spectrum using chosen nscale and fixed nscale Jacobian
-        interp_func = CubicHermiteSpline(
-            x=Nscale_map, y=airmass_eval_grid, dydx=nscale_jacobian_fixed_airmass, axis=0
-        )
-        interp_val = interp_func(eval_nscale).reshape(freq_map.shape)
-        
-        return interp_val
-    
+    mod_jacobian = jacob_interp_func(
+        (x.flatten(),y.flatten(),z.flatten())
+    ).reshape(x.shape)
+
+    final_interp_func = CubicHermiteSpline(
+        x=airmass_map if reverse else Nscale_map,
+        y=first_eval,
+        dydx=mod_jacobian,
+        axis=1 if reverse else 0,
+    )
+
+    return final_interp_func(eval_airmass if reverse else eval_nscale)
     
 def dictionarify(Nscale_points, Nscale_map, Nscale_jacobian, 
                 airmass_points, airmass_map, airmass_jacobian, 
