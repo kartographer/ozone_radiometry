0b828d726432aa2464d3c0aeb94a5e4215dc0e2e
Garrett 'Karto' Keating <garrett.keating@cfa.harvard.edu>
2025-04-02 13:06:16 -0400

    More updates, code speed-ups

--- src/load_ac.py
+++ src/load_ac.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 # Outlier detection algorithm
-def moving_median_sliding(arr: np.ndarray, window_size: int, axis = 1):
+def moving_median_sliding(arr: np.ndarray, window_size: int, axis : int = 1, nan_present : bool=False):
     """Compute moving median and MAD of an array along a specified axis
 
     Arguments:
@@ -21,9 +21,13 @@ def moving_median_sliding(arr: np.ndarray, window_size: int, axis = 1):
     # Apply sliding window view to the padded array
     windows = np.lib.stride_tricks.sliding_window_view(padded_arr, window_shape=(window_size,), axis=axis)
     
-    # Compute the sliding median and MAD    
-    median = np.nanmedian(windows, axis = -1)
-    mad    = np.nanmedian(np.abs(windows - median[..., None]), axis=-1)
+    # Compute the sliding median and MAD
+    if nan_present:
+        median = np.nanmedian(windows, axis = -1)
+        mad    = np.nanmedian(np.abs(windows - median[..., None]), axis=-1)
+    else:
+        median = np.median(windows, axis = -1)
+        mad    = np.median(np.abs(windows - median[..., None]), axis=-1)
 
     return median, mad
 
@@ -32,7 +36,7 @@ def stack_antenna_ac(mir_data: object, antenna_num: int, rx_num: int,
                      mad_dev = 5.0, fill_val = np.nan, 
                      normalization = True, return_el = True, 
                      spw_baselining = True, num_good_points = 40,
-                     num_ignore_edge_chans = 1024, return_both_sb_freqs = True):
+                     edge_chans = 1024, return_both_sb_freqs = True):
     """Code to preprocess autocorrelation data from a telescope
 
     Arguments:
@@ -58,7 +62,7 @@ def stack_antenna_ac(mir_data: object, antenna_num: int, rx_num: int,
             Remove linear trend per spectral window? Minimizes jumps between spectral windows in edge channels
         `num_good_points`   :   int
             Number of good (i.e., non-nan and non-inf) channels at beginning and end of spectral window to fit linear trend
-        `num_ignore_edge_chans` :   int
+        `edge_chans` :   int
             Number of edge channels at beginning and end of spectral window to ignore
         `return_both_sb_freqs`  :   bool
             Calculate and return frequency arrays for the LSB and USB
@@ -79,7 +83,7 @@ def stack_antenna_ac(mir_data: object, antenna_num: int, rx_num: int,
 
     #Assume number of channels is uniform throughout spws
     num_chs    = mir_data.ac_data['nch'][0]
-    chan_pos  = np.arange(-num_chs // 2, num_chs // 2)
+    chan_pos  = np.arange(-num_chs // 2, num_chs // 2)[edge_chans:-edge_chans]
 
     #Loop through each spw
     for spw in range(1, num_spws + 1):
@@ -100,24 +104,23 @@ def stack_antenna_ac(mir_data: object, antenna_num: int, rx_num: int,
             lo_freq = mir_data.ac_data['gunnLO'][0]
 
         #Get channel frequencies
-        freq_res_usb = mir_data.ac_data['fres'][0]*1e-3
-
-        #LSB frequencies are mirrored around LO
-        channel_freq_offsets_usb = chan_pos * freq_res_usb
-        channel_freq_offsets_lsb = np.copy(channel_freq_offsets_usb)[::-1]
+        freq_res = mir_data.ac_data['fres'][0]*1e-3
 
         #Cut off edge channels
-        data_stack = np.vstack([item['data'] for item in mir_data.auto_data.values()])[:, num_ignore_edge_chans:-num_ignore_edge_chans]
+        data_stack = np.vstack(
+            [item['data'][edge_chans:-edge_chans] for item in mir_data.auto_data.values()]
+        )
         
         #Fsky measures center of SPW, offset from LO by constant delta = Fsky - LO
         #LSB for partner SPW is delta below LO, so Fsky' = LO - delta = 2 LO - Fsky
-        #Channel offsets are also mirrored, so add mirrorer channel offset from above to get frequencies per SPW in both SBs
-        f_sky_usb      = mir_data.ac_data['fsky'][0] + channel_freq_offsets_usb[num_ignore_edge_chans:-num_ignore_edge_chans]
-        f_sky_lsb      = 2 * lo_freq - mir_data.ac_data['fsky'][0] + channel_freq_offsets_lsb[num_ignore_edge_chans:-num_ignore_edge_chans]
+        #Channel offsets are also mirrored, so add mirrorer channel offset from above
+        # to get frequencies per SPW in both SBs
+        f_sky_usb      = mir_data.ac_data['fsky'][0] + (chan_pos * freq_res)
+        f_sky_lsb      = (2 * lo_freq) - f_sky_usb
 
         #Flag outliers before concatenation
         if flagging:
-            med, mad = moving_median_sliding(data_stack, window_size, 1)
+            med, mad = moving_median_sliding(data_stack, window_size, 1, nan_present=np.any(np.isnan(data_stack)))
 
             out_of_bounds = np.logical_or(np.less_equal(data_stack, med - mad_dev * mad), 
                                           np.greater_equal(data_stack, med + mad_dev * mad))
@@ -126,9 +129,8 @@ def stack_antenna_ac(mir_data: object, antenna_num: int, rx_num: int,
         
         #Normalize -- what exactly is happening here?
         if normalization:
-            data_stack = data_stack/np.repeat(np.nanmean(data_stack,axis=0).reshape([1,-1]), data_stack.shape[0], axis=0)
-            data_stack = data_stack/np.repeat(np.nanmean(data_stack,axis=1).reshape([-1,1]), data_stack.shape[1], axis=1)
-
+            data_stack /= np.nanmean(data_stack,axis=0)[None]
+            data_stack /= np.nanmean(data_stack,axis=1)[:, None]
             data_stack -= np.nanmean(data_stack)
 
         #Subtract linear baseline per spectral window 
@@ -141,32 +143,29 @@ def stack_antenna_ac(mir_data: object, antenna_num: int, rx_num: int,
 
                 baseline_params = np.poly1d( np.polyfit(good_channels, data_stack[integ, good_channels], 1) )
                 data_stack[integ, :] -= baseline_params(channel_numbers)
-            
+        
+        data_stack = data_stack[:, None, :]
+        if return_both_sb_freqs:
+            f_sky = np.vstack((f_sky_lsb, f_sky_usb))[None]
+        else:
+            f_sky = f_sky_usb[None, None]
+
         #Concatenate processed SPW ACs together
         if spw == 1:
-            stacked = 1 * data_stack
-            freqs_usb = 1 * f_sky_usb
-            freqs_lsb = 1 * f_sky_lsb
-            #spw_num = spw * np.ones(f_sky.shape)
-
+            stacked = data_stack
+            freqs = f_sky
             if return_el:
                 elevation = mir_data.eng_data['actual_el']
 
         else:
-            stacked = np.dstack((stacked, data_stack))
-            freqs_usb = np.dstack((freqs_usb, f_sky_usb))
-            freqs_lsb = np.dstack((freqs_lsb, f_sky_lsb))
-            #spw_num = np.hstack((spw_num, spw * np.ones(f_sky.shape)))
+            stacked = np.concatenate((stacked, data_stack), axis=1)
+            freqs = np.concatenate((freqs, f_sky), axis=0)
 
         #Get ready for the next spw
         mir_data.reset()
 
     #Return elevation and SB freqs if needed
     if return_el:
-        if return_both_sb_freqs:
-            return freqs_lsb, freqs_usb, stacked, elevation
-        return freqs_usb, stacked, elevation
-    
-    if return_both_sb_freqs:
-        return freqs_lsb, freqs_usb, stacked
-    return freqs_usb, stacked
+        return freqs, stacked, elevation
+    else:    
+        return freqs, stacked
