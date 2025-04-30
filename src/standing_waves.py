deeb74ab3e2bcec7383a1b67f3f564f7542479b2
Joe Michail <josephmichail@gmail.com>
2025-04-30 15:22:20 -0400

    Added Legendre polynomial baseline fitting ability, initial ozone modeling into coefficients + returning their value

--- src/standing_waves.py
+++ src/standing_waves.py
@@ -1,10 +1,12 @@
 import numpy as np
 import scipy.linalg as linalg
+from scipy.special import eval_legendre
 
 def remove_standing_waves(stacked_data: np.ndarray, sky_freqs : np.ndarray, 
                          f0_s : np.ndarray, n_harmonics : int, freq_order : int, 
-                         baseline_order : int, fft_window : float = 0.05 / 63.2, 
-                         fft_resolution_multiplier : int = 3):
+                         baseline_order : int, pwv_jacobian : np.ndarray,
+                         return_delta_pwv : bool = True, fft_window : float = 0.05 / 63.2, 
+                         fft_resolution_multiplier : int = 3, legendre : bool = True):
     """Fourier-based function to remove standing wave contribution from autocorrelation data
        and simulatneously fit a per-spw baseline to the data
 
@@ -21,11 +23,17 @@ def remove_standing_waves(stacked_data: np.ndarray, sky_freqs : np.ndarray,
             Polynomial order to expand standing wave contribution in least-squares modeling
         `baseline_order`    :   int
             Polynomial order to fit for spectral baseline per spw
+        `pwv_jacobian`  :   np.ndarray
+            2D Array of (normalized) PWV Jacobians for fit
+        `return_delta_pwv`  :   bool
+            Return array of delta PWV?
         `fft_window`    :   float
             Size of window around f0 * harmonics to search for maximal FFT peak
         `fft_resolution_multiplier` :   int
             Per-spw data are automatically padded to the next highest power of 2; this argument increases FFT spectral
             resolution by additional factors of 2
+        `legendre`  :   bool
+            If True, Legendre polynomials are used to fit the SPW baseline. If false, a polynomial expansion is used. 
     """
     # Make local copy of data to work with
     data = 1 * stacked_data
@@ -62,23 +70,29 @@ def remove_standing_waves(stacked_data: np.ndarray, sky_freqs : np.ndarray,
         freqs_pad = np.vstack((freqs_pad, sky_freqs[spw, 0] + channel_holds * np.diff(sky_freqs[spw, :]).flatten()[0] * channel_steps))
 
     # Normalized sky frequencies for faster convergence later
-    f_sky_norm = 2 * (freqs_pad - np.min(freqs_pad[:, pad_slice])) / (np.max(freqs_pad[:, pad_slice]) - np.min(freqs_pad[:, pad_slice])) - 1
-    f_sky_norm_real = f_sky_norm[:, left_pad : n_chan + left_pad]
+    f_sky_norm = 2 * (sky_freqs - np.nanmin(sky_freqs)) / (np.nanmax(sky_freqs) - np.nanmin(sky_freqs)) - 1
 
     # Best-fit model and cleaned spectrum arrays
     model, cleaned = np.zeros(stacked_data.shape), np.zeros(stacked_data.shape)
 
     # Stores basis for LSQ
     # n_spw * n_chan (axis 0): total number of channels in fit
-    # (baseline_order + 1) * n_spw + 2 * (freq_order + 1): per-spw baselining terms + standing wave coefficients
-    A = np.zeros((n_spw * n_chan, (baseline_order + 1) * n_spw + 2 * (freq_order + 1) * len(f0_list)))
-    
+    # (baseline_order + 1) * n_spw + 2 * (freq_order + 1) + 1: per-spw baselining terms + standing wave coefficients + dPWV term
+    A = np.zeros((n_spw * n_chan, (baseline_order + 1) * n_spw + 2 * (freq_order + 1) * len(f0_list) + 1))
+
     # Build corner of A for independent spw-baseline fitting
     # Integration independent! So do it once only to speed up
     for spw in range(n_spw):
         for n in range(baseline_order + 1):
-            A[spw * n_chan : (spw + 1) * n_chan, (baseline_order + 1) * spw + n] = 1 * f_sky_norm_real[spw, :]**n
+            if legendre:
+                A[spw * n_chan : (spw + 1) * n_chan, (baseline_order + 1) * spw + n] = eval_legendre(n, f_sky_norm[spw, :])
+            else:
+                A[spw * n_chan : (spw + 1) * n_chan, (baseline_order + 1) * spw + n] = 1 * f_sky_norm[spw, :]**n
     
+    # Stores best-fit dPWV
+    if return_delta_pwv:
+        dpwv = np.array([])
+
     # Go through each integration in the cube
     for integ in range(n_integ):
         padded_integ = np.copy(data[integ, :])
@@ -117,9 +131,12 @@ def remove_standing_waves(stacked_data: np.ndarray, sky_freqs : np.ndarray,
             
             #Add terms into basis matrix
             for n in range(freq_order + 1):
-                A[:, (baseline_order + 1) * n_spw + 2 * idx * (freq_order + 1) + 2 * n]     = model_im.flatten() * f_sky_norm_real.flatten()**n
-                A[:, (baseline_order + 1) * n_spw + 2 * idx * (freq_order + 1) + 2 * n + 1] = model_re.flatten() * f_sky_norm_real.flatten()**n
-                
+                A[:, (baseline_order + 1) * n_spw + 2 * idx * (freq_order + 1) + 2 * n]     = model_im.flatten() * f_sky_norm.flatten()**n
+                A[:, (baseline_order + 1) * n_spw + 2 * idx * (freq_order + 1) + 2 * n + 1] = model_re.flatten() * f_sky_norm.flatten()**n
+            
+            # Add Jacobian term
+            A[:, -1] = 1 * pwv_jacobian[i, :]
+
         #Fit the LSQ
         res = linalg.lstsq(A, data[integ, :].flatten())[0]
 
@@ -127,9 +144,15 @@ def remove_standing_waves(stacked_data: np.ndarray, sky_freqs : np.ndarray,
         model[integ, :] = np.matmul(A, res).reshape(data[integ, :].shape)
         cleaned[integ, :] = data[integ, :] - model[integ, :]
 
+        # Store dPWV term if necessary
+        if return_delta_pwv:
+            dpwv = np.append(dpwv, res[-1])
+            
     #Reflag bad data
     model[nan_flag] = np.nan
     cleaned[nan_flag] = np.nan
 
     #Return model and cleaned spectral cubes
+    if return_delta_pwv:
+        return model, cleaned, dpwv
     return model, cleaned
